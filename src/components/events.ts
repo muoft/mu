905c05c70957c827578e2d4764bcbd631991b2fb
bradley <16675719+B44ken@users.noreply.github.com>
2026-03-27 21:12:12 -0400

    remove old commented events

--- src/components/events.ts
+++ src/components/events.ts
@@ -23,28 +23,4 @@ Tickets: $26 + Tax.
 There are a very limited number of tickets, get yours as soon as possible!
     `
     }
-    //     {
-    //         title: `Dog of indomitable hope and optimism`,
-    //         image: `/event-images/dog.png`,
-    //         date: `2025-11-11`,
-    //         location: `Somewhere`,
-    //         body: `I like this dog. He seems to have an indomitable hope and optimism about him. I choose to believe that no matter what life throws at him, he will always find a way to keep going and keep smiling. This is a quality that I admire greatly and strive to embody in my own life.`
-    //     }, {
-    //         title: `Pi Day Game Night`,
-    //         image: `/event-images/dog.png`,
-    //         date: `2025-11-11`,
-    //         location: `Somewhere Else`,
-    //         body: `
-    // Join us tomorrow 4-6pm in BA6183 for our annual Pi Day celebration! Socialize with your fellow math students over games and pies.
-    // 📅 Date: March 14
-    // ⏰ Time: 4–6 pm
-    // 📍 Location: BA6183 (seminar room)
-    // `
-    //     }, {
-    //         title: `Putnam Training Day`,
-    //         image: `/event-images/buttons.png`,
-    //         date: `Saturday`,
-    //         location: `Math Lounge`,
-    //         body: `We will be solving Putnam problems all day in the math lounge as preparation for the Putnam happening this Saturday. Copious amounts of coffee, problems, chalk, and coffee will be provided.`
-    //     }
 ]
